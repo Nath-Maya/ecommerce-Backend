55899c27db2b543e34c60bc2e8da7cdcf99947ca
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-09-11 23:18:08 -0500

    Agregando producto al array products.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import ProductManager from "./controllers/ProductManager.js";
+
+const product = new ProductManager();
+
 
 const app = express();
 const PORT = 8080;
@@ -7,8 +11,20 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-//LEVANTAR SERVIDOR
 
+
+
+app.post("/products", async (req, res) => {
+   let newProduct = req.body
+   res.send(await product.writeProducts(newProduct));
+   
+})
+
+
+
+
+
+//LEVANTAR SERVIDOR
 app.listen(PORT, () => {
    console.log("\u001b[1;35m Servidor express Puerto: " + PORT);
 });
--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -3,22 +3,31 @@ import { promises as fs } from "fs"
 
 class ProductManager {
   constructor() {
-    this.path = "./src/models/products.json"; //Crear la ruta del archivo
+    this.path = "./src/models/products.json" //Crear la ruta del archivo
   }
 
   //! -----INGRESAR LOS PRODUCTOS EN EL ARREGLO.
 
   writeProducts = async (product) => {
-    //Leer los arreglos en el arreglo de productos
+    ;
+
+    try {
+      //Leer los arreglos en el arreglo de productos
     let products = await fs.readFile(this.path, 'utf-8');
     //se debe parsear
     let productsParse = JSON.parse(products); //archivo json
     let allProducts = [...productsParse, product];
     await fs.writeFile(this.path, JSON.stringify(allProducts));
-    return "Producto agregado";
+    return "Producto agregado"
+      
+    } catch (error) {
+      console.error("Error al ingresar al archivo de productos: ",error);
+      return "Error al agregar producto";
+    }
+
   };
 }
 
-const product = new ProductManager
+export default ProductManager; 
 
-product.writeProducts()
+
