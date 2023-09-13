a016bb05631a14bbecab6415a1cf2824fdbe14d1
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-09-12 20:42:24 -0500

    Configurar nanoid

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -1,57 +1,35 @@
-//Se inicia con la importacion de las promesas
 import { promises as fs } from "fs";
+import { nanoid } from "nanoid"; //genera ids automaticos
 
 class ProductManager {
   constructor() {
     this.path = "./src/models/products.json"; //Crear la ruta del archivo
   }
 
-  //metodo para leer los productos.
   readProducts = async () => {
-     //Leer los arreglos en el arreglo de productos
-     let products = await fs.readFile(this.path, "utf-8");
-     //se debe parsear
-     return  JSON.parse(products); //archivo json
+    let products = await fs.readFile(this.path, "utf-8"); //Leer los arreglos en el arreglo de productos
+    return JSON.parse(products);
   };
 
-  writeProducts = async(product) => {
+  writeProducts = async (product) => {
     await fs.writeFile(this.path, JSON.stringify(product));
   };
-
 
   //! -----INGRESAR LOS PRODUCTOS EN EL ARREGLO.
 
   addProducts = async (product) => {
     let productsOld = await this.readProducts();
-    try {
-      let allProducts = [...productsOld, product];
-      await this.readProducts(allProducts);
-      return "Producto agregado";
-    } catch (error) {
-      console.error("Error al ingresar al archivo de productos: ", error);
-      return "Error al agregar producto";
-    }
+    product.id = nanoid(5); //El numero que entra como parametro determina la cant de digitos del id.
+    let allProducts = [...productsOld, product];
+    await this.writeProducts(allProducts);
+    return "Producto agregado";
   };
 
   //!-----GET PRODUCTS
-  //Devolver los productos que estan en el json. 
 
   getProducts = async () => {
     return await this.readProducts();
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+}
 
 export default ProductManager;
