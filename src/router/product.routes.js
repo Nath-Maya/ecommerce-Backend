8f83631d351d83540885d6c2cf38bc751426baec
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-09-14 18:34:22 -0500

    Configurando limit

--- src/router/product.routes.js
+++ src/router/product.routes.js
@@ -5,9 +5,31 @@ const ProductRouter = Router();
 const product = new ProductManager();
 
 //Consulta de todos los productos
+/*
 ProductRouter.get("/", async (req, res) => {
   res.send(await product.getAllProducts());
 });
+*/
+
+// Consulta de todos los productos con límite opcional
+ProductRouter.get("/", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit);
+    console.log("Limit:", limit); // Verifica si se recibe el valor del parámetro "limit"
+
+    const products = await product.getAllProducts();
+
+    if (!isNaN(limit)) {
+      res.send(products.slice(0, limit));
+    } else {
+      res.send(products);
+    }
+  } catch (error) {
+    console.error("Error en la ruta GET /api/products:", error);
+
+  }
+});
+
 
 
 //Consulta del producto con determinado id
