cd0f58faf2e1f1a6f5ca113d3ac1ec48d825f8ef
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-17 20:18:17 -0500

    Add view detail product

--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -1,25 +1,36 @@
 import { Router } from "express";
-import Products from "../DAO/dbManager/products.js"
+import Products from "../DAO/dbManager/products.js";
 
 const viewRouter = Router();
 
-const productsManager = new Products();
+const productManager = new Products();
 
-//! VISTA PRODUCTOS
-viewRouter.get('/',async(req,res)=>{
-    let products = await productsManager.getAllProducts();
-    res.render('home',{products})
-})
+//! VISTA PRODUCTS
+viewRouter.get("/", async (req, res) => {
+  let products = await productManager.getAllProducts();
+  res.render("home", { products });
+});
 
 //! VISTA CHATS
-viewRouter.get('/chats',async(req,res)=>{
-    res.render('chats')
+viewRouter.get("/chats", async (req, res) => {
+  res.render("chats");
+});
+
+//!VISTA CARTS
+viewRouter.get("/carts", async (req, res) => {
+  res.render("carts");
+});
+
+//? VISTA DETAIL PRODUCT
+viewRouter.get("/product/:idProduct", async(req, res) => {
+    let idProduct  = req.params.idProduct;
+    try {
+      let result = await productManager.getProductId(idProduct);
+      res.render('product', result)
+    } catch (error) {
+      console.error("Producto no encontrado");
+    }
 })
 
-//!VISTA CARRITOS
-viewRouter.get('/carts', async (req,res) => {
-    res.render('carts')
-
-})
 
 export default viewRouter;
