b2ae20198126789f1d949ea769cc9e8a6e47fa39
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-22 14:38:00 -0500

    correction of route register

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", (e) => {
   const obj = {};
   data.forEach((value, key) => (obj[key] = value));
 
-  fetch("/register", {
+  fetch("/session/register", {
     method: "POST",
     body: JSON.stringify(obj),
     headers: {
