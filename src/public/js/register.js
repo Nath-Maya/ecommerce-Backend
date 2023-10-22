d5ea9ec880b4ca00cf5e2cc80951669a7db55a6b
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-22 14:19:20 -0500

    Redirect the view profile

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", (e) => {
   const obj = {};
   data.forEach((value, key) => (obj[key] = value));
 
-  fetch("/session/register", {
+  fetch("/register", {
     method: "POST",
     body: JSON.stringify(obj),
     headers: {
