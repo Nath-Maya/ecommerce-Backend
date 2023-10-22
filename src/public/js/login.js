83c9fbe9ab060df9ff4075f44da525b6317c463d
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-22 15:24:02 -0500

    Pending bringing user data into view

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", (e) => {
     },
   }).then((result) => {
     if (result.status === 200) {
-      window.location.replace("/profile");
+      window.location.replace("/products");
     }
   });
 });
