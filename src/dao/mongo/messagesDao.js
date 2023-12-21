4417a7cb04df91d98e79608b057d058ae6892e13
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-12-20 20:29:18 -0500

    applying error handling in messagesDao

--- src/dao/mongo/messagesDao.js
+++ src/dao/mongo/messagesDao.js
@@ -1,18 +1,25 @@
-import messageModel from "../models/messages.model.js"
+import messageModel from "../models/messages.model.js";
 
 export default class MessageDAO {
   constructor() {}
 
-
   sendMessage = async (message) => {
-    let result = await messageModel.create(message);
-    console.log("\u001b[1;36m Mensaje guardado");
-    return result;
+    try {
+      let result = await messageModel.create(message);
+      return result;
+    } catch (error) {
+      console.error("Error al guardar el mensaje:", error.message);
+      throw new Error("Error al guardar el mensaje");
+    }
   };
 
-
-  getAllMessage = async () => {
-    let result = await messageModel.find().lean();
-    return result;
+  getAllMessages = async () => {
+    try {
+      let result = await messageModel.find().lean();
+      return result;
+    } catch (error) {
+      console.error("Error al obtener mensajes:", error.message);
+      throw new Error("Error al obtener mensajes");
+    }
   };
 }
