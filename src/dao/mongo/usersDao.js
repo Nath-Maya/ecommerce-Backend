4d8763db5a433030c0414e8c2cdf38b16ce73710
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-12-20 20:30:23 -0500

    applying error handling in usersDao

--- src/dao/mongo/usersDao.js
+++ src/dao/mongo/usersDao.js
@@ -4,10 +4,14 @@ export default class UsersDAO {
   constructor() {}
 
   getAllUsers = async () => {
-    let result = await userModel.find().lean();
-    return result;
+    try {
+      let result = await userModel.find().lean();
+      return result;
+    } catch (error) {
+      console.error("Error al obtener usuarios:", error.message);
+      throw new Error("Error al obtener usuarios");
+    }
   };
-
 
   getUserById = async (idUser) => {
     try {
@@ -15,21 +19,30 @@ export default class UsersDAO {
       console.log("\u001b[1;36m User found ");
       return user;
     } catch (error) {
-      console.log("\u001b[1;31m User not found");
+      console.error("Error al obtener usuario por ID:", error.message);
+      throw new Error("Error al obtener usuario por ID");
     }
   };
 
-
   saveUser = async (user) => {
-    let result = await userModel.create(user);
-    console.log("\u001b[1;36m User create sucess");
-    return result;
+    try {
+      let result = await userModel.create(user);
+      console.log("\u001b[1;36m User create success");
+      return result;
+    } catch (error) {
+      console.error("Error al crear usuario:", error.message);
+      throw new Error("Error al crear usuario");
+    }
   };
 
-
   deleteUser = async (idUser) => {
-    let result = await userModel.findByIdAndDelete(idUser);
-    console.log("\u001b[1;31m User Deleted");
-    return result;
+    try {
+      let result = await userModel.findByIdAndDelete(idUser);
+      console.log("\u001b[1;31m User Deleted");
+      return result;
+    } catch (error) {
+      console.error("Error al eliminar usuario:", error.message);
+      throw new Error("Error al eliminar usuario");
+    }
   };
 }
